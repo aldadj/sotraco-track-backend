c4cc70379f805da98ed0aa4710810159ac2ccde9
Aldadj Tech <alisnybelem@gmail.com>
2026-08-05 22:01:54 +0000

    Create SOTRACO Track homepage form

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminBusController;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
 // Groupe de routes pour l'administration des bus
